257289b1626f969ac8198f32beee2c8459c544d2
Daniel Vianna <dmlvianna@gmail.com>
2017-11-23 08:58:15 +1100

    working, with pipes-text input. Really cool.

--- src/Patents.hs
+++ src/Patents.hs
@@ -6,41 +6,17 @@
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE TypeOperators     #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module Patents where
 
-import           Control.Applicative
-import qualified Control.Foldl         as L
-import qualified Data.Foldable         as F
-import           Data.Proxy            (Proxy (..))
-import           Data.Tuple
+import           Pipes         (Producer, (>->))
+import qualified Pipes.Prelude as P
 
-import           Data.Vinyl.Lens
-import           Lens.Micro            ((%~))
-import           Lens.Micro.Extras     (view)
-import           Pipes                 (Pipe, Producer, mzero, runEffect, (>->))
-import qualified Pipes.Prelude         as P
-
-import qualified Data.Char             as C
-import           Data.Readable
-import           Data.Text             hiding (Text, take)
-import qualified Data.Text             as T
-import           Data.Typeable
-import qualified Data.Vector           as V
-import           Data.Vinyl            (Rec)
-import           Frames                ((:->), Text)
-import           Frames.ColumnUniverse
-import           Frames.CSV            (colQ, columnNames, columnUniverse,
-                                        declareColumn, readTableMaybe,
-                                        readTableOpt, rowGen, rowTypeName,
-                                        separator, tablePrefix, tableTypes')
-import           Frames.Exploration    (pipePreview, pr, select)
-import           Frames.Frame
-import           Frames.InCore
-import           Frames.Melt
+import           Data.Vinyl    (Rec)
+import           Frames        ((:->), MonadSafe, Text, runSafeEffect)
+import           Frames.CSV    (declareColumn, pipeTableMaybe, readFileLatin1Ln)
 import           Frames.Rec
-import           Frames.RecF
-
 import           PatAbstracts
 
 declareColumn "patId" ''Text
@@ -49,12 +25,12 @@ type PatColumns = '["id" :-> Text, "abstract" :-> Address]
 type PA = Record PatColumns
 type PAMaybe = Rec Maybe PatColumns
 
-patStreamM :: Producer PAMaybe IO ()
-patStreamM = readTableMaybe "data/pat_abstracts.csv"
--- patStreamM = readTableMaybe "../data/IPGOD.IPGOD122B_PAT_ABSTRACTS.csv"
+patStreamM :: MonadSafe m => Producer PAMaybe m ()
+-- patStreamM = readFileLatin1Ln "data/pat_abstracts.csv" >-> pipeTableMaybe
+patStreamM = readFileLatin1Ln "../data/IPGOD.IPGOD122B_PAT_ABSTRACTS.csv" >-> pipeTableMaybe
 
 
 printValidAddresses :: IO ()
 printValidAddresses =
-  runEffect $ patStreamM >-> P.map recMaybe >-> P.concat >-> P.print
+  runSafeEffect $ patStreamM >-> P.map recMaybe >-> P.concat >-> P.print
 
