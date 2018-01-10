9c50aaeaff3266a090ea44f45ba1d38b2dd64b0f
Daniel Vianna <dmlvianna@gmail.com>
2018-01-10 16:53:42 +1100

    boolean subsetting with record accessors

--- src/Patents.hs
+++ src/Patents.hs
@@ -11,18 +11,21 @@
 module Patents where
 
 import           Addresses
-import           Pipes             (Pipe, Producer, (>->))
-import           Pipes.Internal    (Proxy)
-import qualified Pipes.Prelude     as P
+import           Control.Monad.Catch    (MonadMask)
+import           Control.Monad.IO.Class (MonadIO)
+import           Pipes                  (Pipe, Producer, (>->))
+import           Pipes.Internal         (Proxy)
+import qualified Pipes.Prelude          as P
 
-import           Data.Vinyl        (Rec)
+import           Data.Vinyl             (Rec)
 import           Data.Vinyl.Lens
-import           Frames            ((:->), MonadSafe, Text, runSafeEffect)
-import           Frames.CSV        (declareColumn, pipeTableMaybe,
-                                    readFileLatin1Ln)
+import           Frames                 ((:->), MonadSafe, SafeT, Text,
+                                         runSafeEffect)
+import           Frames.CSV             (declareColumn, pipeTableMaybe,
+                                         readFileLatin1Ln)
 import           Frames.Rec
-import           Lens.Micro.Extras (view)
-import           PatAbstracts      ()
+import           Lens.Micro.Extras      (view)
+import           PatAbstracts           ()
 
 declareColumn "patId" ''Text
 declareColumn "abstract" ''AuAddress
@@ -43,12 +46,42 @@ printValidAddresses =
 isAuAddress :: AuAddress -> Bool
 isAuAddress (AuAddress _ _) = True
 
-addresses :: (Abstract ∈ rs, Monad m) => Pipe (Record rs) (Record rs) m r
-addresses = P.filter (isAuAddress . view abstract)
+auAddresses :: (Abstract ∈ rs, Monad m) => Pipe (Record rs) (Record rs) m r
+auAddresses = P.filter (isAuAddress . view abstract)
 
 deMaybe :: Monad m => Proxy () (Rec Maybe cs) () (Record cs) m r
 deMaybe = P.map recMaybe >-> P.concat
 
 runMaybeTake :: Int -> IO ()
 runMaybeTake n =
-  runSafeEffect $ patStreamM >-> deMaybe >-> addresses >-> P.take n >->  P.print
+  runSafeEffect $
+  patStreamM >->
+  deMaybe >->
+  auAddresses >->
+  P.take n >->
+  P.print
+
+-- further modularising
+
+patStreamTake :: (Show b, MonadMask m, MonadIO m) => Proxy () (Record PatColumns) () b (SafeT m) () -> Int -> m ()
+patStreamTake target n =
+  runSafeEffect $
+  patStreamM >->
+  deMaybe >->
+  target >->
+  P.take n >->
+  P.print
+
+isStreetAddress :: AddressLocation -> Bool
+isStreetAddress (AStreetAddress _) = True
+isStreetAddress _                  = False
+
+streetAddresses :: (Abstract ∈ rs, Monad m) => Pipe (Record rs) (Record rs) m r
+streetAddresses = P.filter (isStreetAddress . _addrLocation . view abstract)
+
+vicAddr :: (Abstract ∈ rs, Monad m) => Pipe (Record rs) (Record rs) m r
+vicAddr = P.filter ((== (State "vic")) . _state . _addrLocality . view abstract)
+
+-- patStreamTake vicAddr 3 -- will render Victorian addresses
+-- patStreamTake streetAddresses 3 -- will render Street addresses
+-- patStreamTake (streetAddresses >-> vicAddr) 3 -- the intersection
