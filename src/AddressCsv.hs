5cdb68cca63cdd4e42cca80d020ec7eaaec563d2
Daniel Vianna <dmlvianna@gmail.com>
2018-01-15 19:03:47 +1100

    flattening nested types

--- src/AddressCsv.hs
+++ src/AddressCsv.hs
@@ -15,47 +15,13 @@ import           Addresses
 import           Data.Functor.Identity
 import qualified Pipes.Prelude         as P
 
+import           Data.Vinyl
 import           Frames                ((:->), pattern Nil, Text)
 import           Frames.CSV            (produceCSV)
 import           Frames.Rec
 import           PatAbstracts          ()
-import           Patents
 
-flattenLocality :: Locality -> Record
-  '[ "suburb" :-> Suburb
-   , "state" :-> State
-   , "postcode" :-> Postcode ]
-flattenLocality x = _suburb x &: _state x &: _postcode x &: Nil
-
-dummyAddressRec :: AuAddress
-                -> Maybe (Record '[ "Box" :-> Text, "PostCode" :-> Text ])
-dummyAddressRec x =
-  case _addrLocation x of
-    APobox (Gpo b) -> Just ("GPO" &: b &: Nil)
-    APobox (Po b)  -> Just ("PO"  &: b &: Nil)
-    _              -> Nothing
-
-dAddressRec :: AuAddress
-            -> Maybe (Record
-                      '[ "boxtype" :-> Text
-                       , "boxnumber" :-> Box
-                       , "suburb" :-> Suburb
-                       , "state" :-> State
-                       , "postcode" :-> Postcode
-                       ])
-dAddressRec AuAddress
-  { _addrLocation = x
-  , _addrLocality = y } =
-  case x of
-    APobox (Gpo boxnumber) ->
-      Just $ "GPO" &: boxnumber &: flattenLocality y
-    APobox (Po  boxnumber) ->
-      Just $ "PO"  &: boxnumber &: flattenLocality y
-    -- AStreetAddress x -> undefined
-    _              -> Nothing
-
-dummyAddressCSV :: AuAddress -> [String]
-dummyAddressCSV = runIdentity . P.toListM . produceCSV . dAddressRec
+-- sample data
 
 tPobox :: AuAddress
 tPobox = AuAddress { _addrLocation = APobox (Gpo "1285")
@@ -78,6 +44,8 @@ tAddr = AuAddress {_addrLocation =
                                              , _postcode = Postcode "3000"}
                   }
 
+-- flatten functions
+
 flattenStreetNumber :: StreetNumber
                     -> Record
                     '[ "sPrefix" :-> Prefix
@@ -94,3 +62,80 @@ flattenStreetNumber x =
       Prefix mempty &: Number mempty &: Suffix mempty &: Nil
     Range (Single p n s) (Single p' n' s') ->
       p &: n &: s &: p' &: n' &: s' &: Nil
+
+flattenStAddr :: StreetAddress
+              -> Record
+              '[ "sPrefix" :-> Prefix
+               , "sNumber" :-> Number
+               , "sSuffix" :-> Suffix
+               , "ePrefix" :-> Prefix
+               , "eNumber" :-> Number
+               , "eSuffix" :-> Suffix
+               , "streetName" :-> StreetName
+               , "streetType" :-> StreetType
+               ]
+flattenStAddr (StAddr
+              { _streetNumber = nb
+              , _streetName = sn
+              , _streetType = st
+              }) =
+  flattenStreetNumber nb <+> sn &: st &: Nil
+
+flattenLocality :: Locality
+                -> Record
+                '[ "suburb" :-> Suburb
+                 , "state" :-> State
+                 , "postcode" :-> Postcode
+                 ]
+flattenLocality x = _suburb x &: _state x &: _postcode x &: Nil
+
+-- Maybe for PO box and street addresses
+
+auStAddrRec :: AuAddress
+              -> Maybe (Record
+              '[ "sPrefix" :-> Prefix
+               , "sNumber" :-> Number
+               , "sSuffix" :-> Suffix
+               , "ePrefix" :-> Prefix
+               , "eNumber" :-> Number
+               , "eSuffix" :-> Suffix
+               , "streetName" :-> StreetName
+               , "streetType" :-> StreetType
+               , "suburb" :-> Suburb
+               , "state" :-> State
+               , "postcode" :-> Postcode
+               ])
+auStAddrRec AuAddress
+  { _addrLocation = x'
+  , _addrLocality = y } =
+  case x' of
+    AStreetAddress x ->
+      Just $ flattenStAddr x <+> flattenLocality y
+    _ -> Nothing
+
+auPoboxRec :: AuAddress
+            -> Maybe (Record
+                      '[ "boxtype" :-> Text
+                       , "boxnumber" :-> Box
+                       , "suburb" :-> Suburb
+                       , "state" :-> State
+                       , "postcode" :-> Postcode
+                       ])
+auPoboxRec AuAddress
+  { _addrLocation = x
+  , _addrLocality = y } =
+  case x of
+    APobox (Gpo boxnumber) ->
+      Just $ "GPO" &: boxnumber &: flattenLocality y
+    APobox (Po  boxnumber) ->
+      Just $ "PO"  &: boxnumber &: flattenLocality y
+    _              -> Nothing
+
+-- to csv
+
+streetCSV :: AuAddress -> [String]
+streetCSV = runIdentity . P.toListM . produceCSV . auStAddrRec
+
+poboxCSV :: AuAddress -> [String]
+poboxCSV = runIdentity . P.toListM . produceCSV . auPoboxRec
+
